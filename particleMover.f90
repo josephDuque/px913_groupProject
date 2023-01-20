1bde9e1931562b38f81c9a8b9e9d52f398d8cf79
josephDuque <naejoah.duque@gmail.com>
2023-01-20 01:17:51 +0000

    Commented particleMover.f90

--- particleMover.f90
+++ particleMover.f90
@@ -12,33 +12,33 @@ MODULE subroutines
         CALL parse_args !CALL parse_args function to 'grab and store' arguments specified in command line.
         success = get_arg("Nx", Nx,exists=exists)
         IF(success) THEN
-            IF(Nx <= 0_INT32) THEN    !The number of grid cells in x-direction must be a positive integer. Thus, if a either a negative number or zero are specified, an error relating to this is printed, and the program is stopped.
+            IF(Nx <= 0_INT32) THEN    !Number of grid cells in x-direction must be a positive integer. If Nx <= 0 is supplied, code stops and error prints.
                 PRINT*,'Error: Nx must be set to a positive value'
                 STOP
             END IF
         ELSE
-            IF(exists) THEN
+            IF(exists) THEN !Notify user of incorrect type for Nx. Stops code.
                 PRINT*,"Value for 'Nx' of incorrect type/kind passed, ensure command line input for 'Nx' is suitable for type and&
                  & kind specified by INT32."
                 STOP
-            ELSE
-                PRINT*,"Value for 'Nx' not parsed, check command line input."
+            ELSE        !Notify user no value for Nx was passed. Stops code.
+                PRINT*,"Value for 'Nx' not parsed, check command line input." 
                 STOP
             END IF
         END IF
 
         success = get_arg("Ny", Ny,exists=exists)
         IF(success) THEN
-            IF(Ny <= 0_INT32) THEN    !The number of grid cells in y-direction must be a positive integer. Thus, if a either a negative number or zero are specified, an error relating to this is printed, and the program is stopped.
+            IF(Ny <= 0_INT32) THEN    !Number of grid cells in y-direction must be a positive integer. If Ny <= 0 is supplied, code stops and error prints.
                 PRINT*,'Error: Ny must be set to a positive value'
                 STOP
             END IF
         ELSE
-            IF(exists) THEN
+            IF(exists) THEN !Notify user of incorrect type for Ny. Stops code.
                 PRINT*,"Value for 'Ny' of incorrect type/kind passed, ensure command line input for 'Ny' is suitable for type and&
                  & kind specified by INT32."
                 STOP
-            ELSE
+            ELSE        !Notify user no value for Nx was passed. Stops code.
                 PRINT*,"Value for 'Ny' not parsed, check command line input."
                 STOP
             END IF
@@ -47,11 +47,11 @@ MODULE subroutines
         success = get_arg("rho", rho, exists=exists)
         IF(success) THEN
         ELSE
-            IF(exists) THEN
+            IF(exists) THEN !Notify user of incorrect type for rho. Stops code.
                 PRINT*,"Value for 'rho' of incorrect type passed, ensure command line input for 'init' is one of &
                 &'null','single' or 'double'"
                 STOP
-            ELSE
+            ELSE        !Notify user no value for rho was passed. Stops code.
                 PRINT*,"Value for 'rho' not parsed, check command line input"
                 STOP
             END IF
@@ -59,13 +59,13 @@ MODULE subroutines
 
     END SUBROUTINE params
 
-    
+
     SUBROUTINE null(lower, rho_grid, initPos, initVel)
         INTEGER :: lower
         REAL(REAL64), DIMENSION(2), INTENT(OUT) :: initPos, initVel
         REAL(REAL64), DIMENSION(lower:,lower:), INTENT(INOUT) :: rho_grid
                 
-        rho_grid = 0.0_REAL64
+        rho_grid = 0.0_REAL64       !Generate density field for rho=null condition.
 
         ! Setting initial position and velocity:
         initPos = 0.0_REAL64
@@ -82,10 +82,11 @@ MODULE subroutines
         REAL(REAL64) :: x_div, y_div, z_1
         INTEGER :: i,j
 
-        rho_grid = 0.0_REAL64
-        z_1 = 0.1_REAL64
+        
+        z_1 = 0.1_REAL64    !Variable assigned to minimise risk of inputting incorrect float into equation.
 
-            
+        !Nested loop used to generate density field for rho=single condition.      
+        !Code optimized with column-major favouring loop.
         DO i=1,Nx
             x_div = (x_axis(i)/z_1)**2       
             DO j=1,Ny
@@ -112,11 +113,12 @@ MODULE subroutines
         REAL(REAL64), DIMENSION(lower:), INTENT(IN) :: x_axis, y_axis
         REAL(REAL64) :: x_div, y_div, extra_divx, extra_divy, z_1, z_2
         INTEGER :: i, j
-        
-        z_1 = 0.1_REAL64
-        z_2 = 0.2_REAL64
+        !Variables assigned to minimise risk of inputting incorrect float into equation.
+        z_1 = 0.1_REAL64    
+        z_2 = 0.2_REAL64    
 
-
+        !Nested loop used to generate density field for rho=double condition.      
+        !Code optimized with column-major favouring loop.
         DO i=1,Nx
             x_div = ((x_axis(i)+0.25_REAL64)/z_1)
 
@@ -136,39 +138,40 @@ MODULE subroutines
 
     END SUBROUTINE double
 
-
+    !Subroutine generates phi_grid that satisfies convergence criteria.
     SUBROUTINE phi(lower, phi_grid, Nx, Ny, rho_grid, dx, dy)
         INTEGER, INTENT(IN) :: lower, Nx, Ny
         REAL(REAL64), INTENT(IN) :: dx, dy
         REAL(REAL64), DIMENSION(lower:,lower:), INTENT(INOUT) :: rho_grid, phi_grid
         REAL(REAL64) :: dens, div1, div2, two, denom, N
-        REAL(REAL64) :: etot, conv1, conv2, drms_sum, drms,drms1
-        INTEGER :: i, j, zed
+        REAL(REAL64) :: etot, conv1, conv2, drms_sum, drms
+        INTEGER :: i, j
 
-        two = 2.0_REAL64
+        two = 2.0_REAL64 !Variable assigned to minimise risk of inputting incorrect float into equation.
 
-        etot = 10.0_REAL64
+        !Initialise values of convergence criteria to allow DO WHILE loop to start,
+        etot = 10.0_REAL64 
         drms = 1.0_REAL64
 
-        zed=0_INT32
-        DO i=1,Nx
-            DO j=1,Ny                
-                dens=rho_grid(j,i)                                      
+        
+        DO i=1,Nx                                                           !Calculate one initial set of elements for phi grid.
+            DO j=1,Ny                                                       !ABS() in final line of DO loop initializes grid to give
+                dens=rho_grid(j,i)                                          !positive values of 'dmrs', allowing convergence criteria to work.
                 div1=((phi_grid(j,i+1) + phi_grid(j,i-1))/(dx**2))
                 div2=((phi_grid(j+1,i) + phi_grid(j-1,i))/(dy**2))
                 denom=((two/(dx**2))+(two/(dy**2)))
-                phi_grid(j,i)=(-(ABS(dens-div1-div2)/denom))            !Calculate one initial set of elements for phi grid.
-                                                                        !ABS() in final line of DO loop initializes grid to give           
-            END DO                                                      !positive values of 'dmrs', allowing convergence criteria to work.
+                phi_grid(j,i)=(-(ABS(dens-div1-div2)/denom))            
+                                                                                   
+            END DO                                                      
 
         END DO
 
-
+        !Loop until convergence criteria are met.
         DO WHILE(etot/drms > 0.00001_REAL64)
             
             DO i=1,Nx
-                DO j=1,Ny                
-                    dens=rho_grid(j,i)                                      !Calculates phi values for whole grid once
+                DO j=1,Ny                                                       !Do one cycle of gauss-seidel method to generate new values for phi grid.
+                    dens=rho_grid(j,i)                                          
                     div1=((phi_grid(j,i+1) + phi_grid(j,i-1))/(dx**2))
                     div2=((phi_grid(j+1,i) + phi_grid(j-1,i))/(dy**2))
                     denom=((two/(dx**2))+(two/(dy**2)))
@@ -178,51 +181,34 @@ MODULE subroutines
 
             END DO
 
-            N = REAL(Nx, kind=REAL64)*REAL(Ny, kind=REAL64)    !Make REAL to allow use of N in generation of REALs below.
+            N = REAL(Nx, kind=REAL64)*REAL(Ny, kind=REAL64)    !Make REAL to prevent REAL-INTEGER arithmetic below..
 
+            !(Re-)initialise values for convergence criteria.
             etot = 0.0_REAL64
-            drms_sum = 0.0_REAL64       !(Re-)Initialise values
+            drms_sum = 0.0_REAL64       
             drms = 0.0_REAL64
 
-        
+            !Loop through phi_grid to generate values for convergence criteria.
+            !Generation of new values for convergence criteria after each Gauss-Seidel cycle allows exit from loop as soon as criteria is met.
             DO i=1,Nx
                 DO j=1,Ny
                     
                     dens=rho_grid(j,i)
-                    conv1 = ((phi_grid(j,i-1) - (two*(phi_grid(j,i))) + phi_grid(j,i+1))/(dx**2))
+                    conv1 = ((phi_grid(j,i-1) - (two*(phi_grid(j,i))) + phi_grid(j,i+1))/(dx**2))       
                     conv2 = ((phi_grid(j-1,i) - (two*(phi_grid(j,i))) + phi_grid(j+1,i))/(dy**2))
                     
-                    etot = etot + ABS(conv1 + conv2 - dens)
+                    etot = etot + ABS(conv1 + conv2 - dens) !Use loop to calculate sum for etot.
 
-                    drms_sum = drms_sum + conv1 + conv2
-
-                    !PRINT*,ABS(drms_sum)
-                    !PRINT*,1/N
-                    !PRINT*,(1/N)*(ABS(drms_sum))
+                    drms_sum = drms_sum + conv1 + conv2 !Use loop to calculate the sum part of drms.
                 
                 END DO
             
             END DO
 
-            drms = SQRT((1/N)*((drms_sum)))
-            drms1 = SQRT((1/N)*(ABS(drms_sum)))        
-            zed=zed+1
+            drms = SQRT((1/N)*((drms_sum)))    !After sum part of drms calculated in loop, calculate full drms outside of loop.
+            
         
         END DO
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,'zed=',zed
-        !PRINT*,etot/drms
-        !PRINT*, etot/drms1
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,etot
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,drms_sum
-        !PRINT*,'------------------------------------------------------------------------------'
-        !PRINT*,drms
-   
 
 
     END SUBROUTINE phi
@@ -236,10 +222,11 @@ MODULE subroutines
         REAL(REAL64) :: two
         INTEGER :: i, j
 
-        two = 2.0_REAL64
+        two = 2.0_REAL64    !Variable assigned to minimise risk of inputting incorrect float into equation.
+        !Use central difference to find gradients of phi in x and y directions (x and y electric fields).
         DO i=1,Nx
             Do j=1,Ny
-                E_field_x(j,i) = ((phi_grid(j,i+1) - phi_grid(j,i-1))/(two*dx))
+                E_field_x(j,i) = ((phi_grid(j,i+1) - phi_grid(j,i-1))/(two*dx))     
                 E_field_y(j,i) = ((phi_grid(j+1,i) - phi_grid(j-1,i))/(two*dy))
 
             END DO
@@ -353,22 +340,19 @@ PROGRAM main                !REMEMBER KING, I HAVE INDEXED IN STRANGE WAYS (J,I)
 !-------------------------------------------------Set axes to be indexed later-------------------------------------------
 
     axis_range(1)=-1.0_REAL64
-    axis_range(2)=1.0_REAL64
+    axis_range(2)=1.0_REAL64        !Use of two axis ranges allows correct orientation of axes in plots.
     axis_range2(1)=1.0_REAL64
     axis_range2(2)=-1.0_REAL64
-    CALL create_axis(x_axis, Nx, axis_range, nghosts=1, delta=dx)
-    CALL create_axis(y_axis, Ny, axis_range2, nghosts=1, delta=dy)
+    CALL create_axis(x_axis, Nx, axis_range, nghosts=1, delta=dx)       !Use create_axis subroutine to generate axes with points aligning with centres of elements in grids.
+    CALL create_axis(y_axis, Ny, axis_range2, nghosts=1, delta=dy)      !Used added functionality of calculating dx and dy, as well as including positions of ghost nodes.
 
-    !DO i=0, Nx+1
-    !  PRINT *, x_axis(i)
-    !END DO
 !--------------------------------------------------Generate rho_grid-----------------------------------------------------
     
     lower=0_INT32
     ALLOCATE(rho_grid(0:Ny+1,0:Nx+1))
-    rho_grid = grid
-    IF(rho == 'null' .OR. rho == 'NULL' .OR. rho == 'Null') THEN !It is plausible that users will use upper and lower case values of init interchangeably. 
-      CALL null(lower,rho_grid, pPos, pVel)                    !Thus, the code is written to allow this. 
+    rho_grid = grid     !Initialise rho_grid with values. Does not matter element values subsequently overwritten.
+    IF(rho == 'null' .OR. rho == 'NULL' .OR. rho == 'Null') THEN !It is plausible that users will use upper and lower case versions of state names interchangeably. 
+      CALL null(lower,rho_grid, pPos, pVel)                      
         
         
     ELSE IF(rho == 'single' .OR. rho == 'SINGLE' .OR. rho == 'Single' ) THEN
@@ -382,21 +366,20 @@ PROGRAM main                !REMEMBER KING, I HAVE INDEXED IN STRANGE WAYS (J,I)
     ELSE
       PRINT*,"Error: Invalid state initialisation requested, choose one of 'null', 'single' or 'double'. &
         &Write in lowercase only."           !If no valid state of initialisation is requested, this program will not work as 
-      STOP                                   !intended. Thus, in response to an invalid input, a related error is printed, 
-                                             !and the program is stopped.To minimise confusion, it is suggested to write in 
-                                             !lowercase, even though other cases would work.
+      STOP                                   !intended. In response to an invalid input, an error prints and the code stops running. 
+                                             !An explanation of allowed states is provided.
     END IF
     
     !---------------------------------------Generate phi_grid------------------------------------------------------------
     ALLOCATE(phi_grid(0:Ny+1,0:Nx+1))
-    phi_grid = grid
-    
+    phi_grid = grid     !Initialise phi_grid with boundary of 0 values, and +1 inside. 
+                        !It was suspected this would reduce Gauss-Seidel cycles required for convergence in phi subroutine, in comparison to array of zeros.
     ALLOCATE(E_field_x(Ny,Nx))
     ALLOCATE(E_field_y(Ny,Nx))
     
     
-    CALL phi(lower, phi_grid, Nx, Ny, rho_grid, dx, dy)
-    CALL E_fields(E_field_x, E_field_y, phi_grid, dx, dy, Nx, Ny,lower)
+    CALL phi(lower, phi_grid, Nx, Ny, rho_grid, dx, dy)     !Call subroutine to generate phi_grid that satisfies convergence criteria.
+    CALL E_fields(E_field_x, E_field_y, phi_grid, dx, dy, Nx, Ny,lower)     !Call subroutine to generate arrays containing values for electric fields.
 
 
     !-----------------------------------------------------------------
@@ -449,56 +432,4 @@ END PROGRAM main
 
 
 
-!-------------------Graveyard-----------------------------
 
-    !print*,lbound(rho_grid)
-    !print*,ubound(rho_grid)
-    !PRINT*, rho_grid
-
-
-    !PRINT*,rho_grid
-    !PRINT*,'---------------------------------------------------------------------------'
-    !PRINT*,phi_grid
-    !PRINT*,'---------------------------------------------------------------------------'
-    !Print*,E_field_x
-    !PRINT*,E_field_y
-!   ! Variable format depending on the number of columns.
-!    write (formatName,'(A1,I4,A13)' ) "(",Nx,"(ES23.12,2X))"
-!   ! print*,formatName
-!
-!    open (10 , file = "xE_field.txt" , form = 'formatted')
-!   
-!   
-!    do i = 1, Ny
-!        write (10,formatName) (E_field_x(i, j), j = 1, Nx)
-!    end do 
-!!  !  close(10)
-!
-!    open (11 , file = "yE_field.txt" , form = 'formatted')
-!
-!    !23 FORMAT (3 ( ES23 .12 E3 ) )
-!    do i = 1, Ny
-!        write (11,formatName) (E_field_y(i, j), j = 1, Nx)
-!    end do
-!    
-!    close(10)
-!    close(11)
-!
-    ! Here I thought the specification wanted the arrays outputted with dimensions (Nx, Ny) 
-    ! but when plotting I noticed they were rotated:
-!   ALLOCATE(netcdf_rho(Nx, Ny))
-!   ALLOCATE(netcdf_phi(Nx, Ny))
-!   ALLOCATE(netcdf_Ex(Nx, Ny))
-!   ALLOCATE(netcdf_Ey(Nx, Ny))
-!
-!
-!   ! Transposing the arrays for netcdf output as per the specification:
-!   netcdf_rho = TRANSPOSE(rho_grid(1:Ny, 1:Nx))
-!   netcdf_phi = TRANSPOSE(phi_grid(1:Ny, 1:Nx))
-!   netcdf_Ex = TRANSPOSE(E_field_x(1:Ny, 1:Nx))
-!   netcdf_Ey = TRANSPOSE(E_field_y(1:Ny, 1:Nx))
-!   
-!   DEALLOCATE(netcdf_Ey)
-!   DEALLOCATE(netcdf_Ex)
-!   DEALLOCATE(netcdf_phi)
-!   DEALLOCATE(netcdf_rho)
